c2ca2acc8702af8bcb60ee5e3f6cb60598cee306
FabriRO05 <7b.2018.fabrizio@gmail.com>
2026-06-12 00:33:00 -0300

    firstcommit

---
+++ models/stadium.py
@@ -0,0 +1,27 @@
+from models.db import db
+
+class Stadium(db.Model):
+    __tablename__ = 'stadiums'
+    
+    id_stadium = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    ubication = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    country = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, name, ubication, city, country):
+        self.name = name
+        self.ubication = ubication
+        self.city = city
+        self.country = country
+    
+    def serialize(self):
+        return {
+            'id_stadium': self.id_stadium,
+            'name': self.name,
+            'ubication': self.ubication,
+            'city': self.city,
+            'country': self.country
+        }
+
+    
