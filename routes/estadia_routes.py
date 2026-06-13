47acee12d71562cb4e76f9decf4865dc9edce8fd
bizzottogregorio-dot <bizzottogregorio@gmail.com>
2026-06-12 21:40:12 -0300

    subiendo routes

---
+++ routes/estadia_routes.py
@@ -0,0 +1,34 @@
+from flask import Blueprint,jsonify,request
+from controllers.estadia_controller import *
+
+estadia_bp = Blueprint('estadia',__name__)
+
+@estadia_bp.route('/alojamientos',methods=['GET'])
+def get_alojamientos():
+    return jsonify(get_all_alojamientos())
+
+@estadia_bp.route('/alojamientos/ciudad/<string:ciudad>',methods=['GET'])
+def get_alojamientos_by_ciudad(ciudad):
+    return jsonify(get_ciudad_alojamientos(ciudad))
+
+@estadia_bp.route('/alojamientos/pais/<string:pais>',methods=['GET'])
+def get_alojamientos_by_pais(pais):
+    return jsonify(get_alojamientos_by_pais(pais))
+
+@estadia_bp.route('/alojamientos/id/<int:id>',methods=['GET'])
+def get_alojamientos_by_id(id):
+    return jsonify(get_alojamiento_by_id(id))
+
+@estadia_bp.route('/alojamientos',methods=['POST'])
+def create_alojamiento():
+    data=request.json
+    create_alojamiento(data)
+    return "alojamiento creado",201
+
+@estadia_bp.route('/alojamientos/<int:id>',methods=['DELETE'])
+def delete_alojamiento(id):
+    try:
+        borrar=delete_alojamiento(id)
+        return jsonify({'mensaje':'alojamiento borrado'})
+    except:
+        return jsonify({'mensaje':'error al borrar el alojamiento'}),404
