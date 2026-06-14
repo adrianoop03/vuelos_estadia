6723cea63c721c12eda2188e53bce57018be8846
Lautaro Tonini <lautarotonini@gmail.com>
2026-06-13 23:12:14 -0300

    creacion extensions

--- app.py
+++ app.py
@@ -3,19 +3,25 @@ import token
 from unicodedata import category
 
 from flask import Flask, render_template
-from config.config import DATABASE_CONNECTION_URI
+from config.config import *
 
 from models.db import db
 from sqlalchemy.exc import OperationalError
 from sqlalchemy_utils import database_exists, create_database
 from flask_mail import Mail
-
-from models.user import User
 from routes.user_route import user
-
+from extensions import mail
 app = Flask(__name__)
 app.register_blueprint(user)
 
+
+app.config['MAIL_SERVER'] = mail_server
+app.config['MAIL_PORT'] = mail_port
+app.config['MAIL_USE_TLS'] = use_tls
+app.config['MAIL_USERNAME'] = mail_username
+app.config['MAIL_PASSWORD'] = mail_password
+
+mail.init_app(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"]= DATABASE_CONNECTION_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
