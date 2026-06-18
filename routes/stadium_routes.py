4237a5cb9ed172240c89f8f082c4f3e24379ac5f
FabriRO05 <7b.2018.fabrizio@gmail.com>
2026-06-18 00:27:03 -0300

    Modificacion de stadium_routes

--- routes/stadium_routes.py
+++ routes/stadium_routes.py
@@ -5,3 +5,11 @@ from TeamTransit.controllers import stadium_controller
 @stadium_controller.route('/stadiums', methods=['GET'])
 def listar_productos():
     return jsonify(stadium_controller.get_all_stadiums())
+
+@stadium_controller.route('/stadiums/<int:id_stadium>', methods=['GET'])
+def obtener_producto(id_stadium):
+    stadium = stadium_controller.get_stadium_by_id(id_stadium)
+    if stadium:
+        return jsonify(stadium)
+    else:
+        return jsonify({'message': 'Estadio no encontrado'}), 404
